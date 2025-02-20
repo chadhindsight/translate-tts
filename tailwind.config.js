bbbfc08354adf4beb2ea70944331ac8e2037326d
chadhindsight <chadrickJ8@gmail.com>
2025-02-20 12:53:20 -0500

    Add a tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    darkMode: 'class', // Enable class-based dark mode
+    content: [
+      './index.html', // Add paths to your HTML files
+      './script.js',  // Add paths to your JS files (if they contain Tailwind classes)
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
